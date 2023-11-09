3d9b4133a7889818a00de36ddab43a20fb9fba14
Andre Pedroso <editorcle@gmail.com>
2023-11-08 23:20:18 -0300

    changes to skypotifolio

--- src/components/SkyPortifolio.vue
+++ src/components/SkyPortifolio.vue
@@ -16,9 +16,6 @@
       <img src="src/assets/images/parallax/cloud3.webp" />
     </div>
     <div class="parallax__layer parallax__layer__5">
-      <!-- <img
-        src="https://github.com/samdbeckham/blog/blob/master/dev/_assets/images/articles/firewatch/layer_5.png?raw=true"
-      /> -->
       <p>Transformando<br />Ideias</p>
     </div>
     <div class="parallax__layer parallax__layer__6">
@@ -56,70 +53,75 @@
     rgba(68, 143, 198, 1) 0%,
     rgba(33, 93, 145, 1) 100%
   );
-}
 
-.parallax__layer {
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-}
-.parallax__layer img,
-.parallax__layer p {
-  display: block;
-  position: absolute;
-  bottom: 0;
-  width: 100%;
-}
-
-.parallax__layer p {
-  color: white;
-  left: 0;
-  text-align: center;
-  font-size: 6rem;
-  right: auto;
-  font-weight: 900;
-  line-height: 0.9;
-  bottom: 72%;
-  text-transform: uppercase;
-}
-.parallax__cover {
-  background: #fff;
-  display: block;
-  position: absolute;
-  top: 100%;
-  left: 0;
-  right: 0;
-  height: 2000px;
-  z-index: 2;
-  box-shadow: 0px -20px 20px 20px #fff;
-  & p {
+  &__layer {
+    position: absolute;
     top: 0;
-    background-attachment: fixed;
-    position: relative;
-    background-image: url("src/assets/images/parallax/cloud-mask.webp");
-    background-size: cover;
-    background-position: bottom center;
-    background-attachment: fixed;
-    background-clip: text;
-    -webkit-background-clip: text;
-    color: transparent;
-    // color: green;
-    left: 0%;
-    text-align: center;
-    font-size: 6rem;
-    right: 50%;
-    font-weight: 900;
-    line-height: 0.9;
-    bottom: 84%;
-    text-transform: uppercase;
-
-    &::before {
-      content: "Em projetos";
-      color: var(--primary);
+    right: 0;
+    bottom: 0;
+    left: 0;
+    & img,
+    & p {
+      display: block;
       position: absolute;
-      z-index: -1;
+      bottom: 0;
+      width: 100%;
+    }
+    & p {
+      color: white;
+      left: 0;
+      text-align: center;
+      font-size: 6rem;
+      right: auto;
+      font-weight: 900;
+      line-height: 0.9;
+      bottom: 72%;
+      text-transform: uppercase;
+    }
+  }
+
+  &__cover {
+    background: #fff;
+    display: block;
+    position: absolute;
+    top: 100%;
+    left: 0;
+    right: 0;
+    height: 2000px;
+    z-index: 2;
+    box-shadow: 0px -20px 20px 20px #fff;
+    & p {
+      top: 0;
+      background-attachment: fixed;
+      position: relative;
+      background-image: url("src/assets/images/parallax/cloud-mask.webp");
+      background-size: cover;
+      background-position: bottom center;
+      background-attachment: fixed;
+      background-clip: text;
+      -webkit-background-clip: text;
+      color: transparent;
+      // color: green;
+      left: 0%;
+      text-align: center;
+      font-size: 6rem;
+      right: 50%;
+      font-weight: 900;
+      line-height: 0.9;
+      bottom: 84%;
+      text-transform: uppercase;
+
+      &::before {
+        content: "Em projetos";
+        color: var(--primary);
+        position: absolute;
+        z-index: -1;
+        opacity: 0.5;
+      }
+    }
+    &-text {
+      position: sticky;
+      top: 6rem;
     }
   }
 }
@@ -146,6 +148,9 @@
 
 .parallax__layer__5 {
   transform: translateZ(-50px) scale(1.5);
+  z-index: -1;
+  position: sticky;
+  top: 16rem;
 }
 
 .parallax__layer__6 {
